7e2457e48964708237811f08fe83fa94eea0b3e8
Gustavo Kazuo <kazuo.hagy@gmail.com>
2025-04-01 23:30:25 -0300

    leitor de live working

--- leitor.py
+++ leitor.py
@@ -5,7 +5,34 @@ from playsound import playsound
 from multiprocessing import Process
 #pt-BR-FranciscaNeural audio femenino
 #pt-BR-AntonioNeural audio masculino
-video_id = "MwEJyxlIhks"  # ID da live
+
+from googleapiclient.discovery import build
+#va no google cloud e cria um projeto, depois ativa a api do youtube e gera uma chave de api
+#https://console.cloud.google.com/apis/library/youtube.googleapis.com
+#ou se preferir pode apenas colocar na variavel video_id2 o id da live
+#e rodar o programa normalmente
+API_KEY = ""
+CHANNEL_ID = ""
+
+def get_live_video_id():
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+    request = youtube.search().list(
+        part="id",
+        channelId=CHANNEL_ID,
+        eventType="live",
+        type="video"
+    )
+    response = request.execute()
+    
+    if "items" in response and len(response["items"]) > 0:
+        return response["items"][0]["id"]["videoId"]
+    
+    return None
+
+live_id = get_live_video_id()
+print(f"ID da Live: {live_id}")
+
+video_id2 = live_id  # ID da live por exemplo: "dQw4w9WgXcQ"
 nome_arquivo = "audio.mp3"  # Sempre o mesmo arquivo
 
 def tocar_audio(arquivo):
@@ -14,7 +41,7 @@ def tocar_audio(arquivo):
 
 async def texto_para_audio(texto):
     """Gera e toca o áudio, substituindo o arquivo anterior."""
-    communicate = edge_tts.Communicate(texto, "pt-BR-AntonioNeural")
+    communicate = edge_tts.Communicate(texto, "pt-BR-FranciscaNeural")
     await communicate.save(nome_arquivo)  # Sempre salva no mesmo arquivo
     print(f"Áudio salvo como {nome_arquivo}")
 
@@ -24,7 +51,7 @@ async def texto_para_audio(texto):
     p.join()  # Aguarda a execução para evitar múltiplas reproduções sobrepostas
 
 if __name__ == '__main__':
-    chat = pytchat.create(video_id)
+    chat = pytchat.create(video_id2)
 
     while chat.is_alive():
         for msg in chat.get().sync_items():
